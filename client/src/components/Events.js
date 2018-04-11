f9f3343492a0bb7605a4434e3617f3723aaeb0ff
Dan Actor <danltactor@gmail.com>
2018-04-10 18:18:57 -0600

    events component
    Reviewers: #events_page, Brockawesome
    
    Reviewed By: #events_page, Brockawesome
    
    Differential Revision: http://phabricator.devpointstudios.com/D1678

---
+++ client/src/components/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+class Events extends Component {
+  
+  render() {
+    return(
+      <Header as="h1" textAlign="center">Events</Header>
+    );
+  }
+}
+
+export default Events;
