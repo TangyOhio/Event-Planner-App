359827e258fc7022af89616e4fe476a45e79b314
Brock Beuch <brockbeuch@gmail.com>
2018-04-10 20:36:16 -0600

    created event card
    Reviewers: #events_page, #staff, JonnyUtah23
    
    Reviewed By: #events_page, JonnyUtah23
    
    Subscribers: JonnyUtah23
    
    Differential Revision: http://phabricator.devpointstudios.com/D1684

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import { List, Header, Card, Container, GridColumn, GridRow } from 'semantic-ui-react';
+import { List, Header, Card, Container, GridColumn, Grid, Image } from 'semantic-ui-react';
 import { Redirect, Link } from 'react-router-dom';
 // import { isAuthenticated } from '../fakeAuth';
+import  Events from './Events';
 
 class EventList extends React.Component {
   state = { events: [] }
@@ -14,23 +15,68 @@ class EventList extends React.Component {
         this.setState({ events: res.data })
     }).catch(err => {
         console.log(err)
-    }) 
+    })
   }
+
+  eventTime = (event) => {
+    return(
+      <Grid centered>
+        <Grid.Row>
+          <Grid.Column width={8} style={{ textAlign: 'center'}}>
+            Date: { event.date}
+
+          </Grid.Column>
+          <Grid.Column width={8} style={{ textAlign: 'center'}}>
+            Time: {event.start_time} - {event.end_time}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+
+
+  displayEvents = () => {
+  return this.state.events.map( event => {
+    return(
+
+      <Card>
+        <Card.Content>
+          <Card.Header>
+            {event.title}
+            <hr />
+            {event.category}
+          </Card.Header>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={16}>
+                <Image src={ event.event_image } />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Card.Description>
+            {event.description}
+          </Card.Description>
+          <Link to ={`/event/${event.id}`}>
+            View Details
+          </Link>
+        </Card.Content>
+        <Card.Content extra>
+          { this.eventTime(event)}
+        </Card.Content>
+      </Card>
+    )
+  })
+}
 
   render() {
     let { events } = this.state;
     return (
       <div>
         <h1>Event List</h1>
-        <ul>
-            { events.map( e =>
-                <li key={e.id}>
-                  <Link to={`/events/${e.id}`}>{e.title}</Link>
-                </li>
-              )
-            }
-          </ul>
-        </div>
+        <Card.Group stackable itemsPerRow={3}>
+          {this.displayEvents()}
+        </Card.Group>
+      </div>
     )
   }
 }
--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,11 +1,73 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { Component, Link } from 'react';
+import { Header, Card, Grid, Segment, Container, Image } from 'semantic-ui-react';
+import axios from 'axios';
 
 class Events extends Component {
-  
+  state = { events: [] }
+
+  componentDidMount() {
+    axios.get('/api/events')
+      .then( res => this.setState({ events: res.data }) )
+    console.log(this.state.events)
+  }
+
+  eventTime = (event) => {
+    return(
+      <Grid centered>
+        <Grid.Row>
+          <Grid.Column width={8} style={{ textAlign: 'center'}}>
+            Date: { event.date}
+
+          </Grid.Column>
+          <Grid.Column width={8} style={{ textAlign: 'center'}}>
+            Time: {event.start_time} - {event.end_time}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+
+
+  displayEvents = () => {
+  return this.state.events.map( event => {
+    return(
+
+      <Card>
+        <Card.Content>
+          <Card.Header>
+            {event.title}
+            <hr />
+            {event.category}
+          </Card.Header>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={16}>
+                <Image src={ event.event_image } />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Card.Description>
+            {event.description}
+          </Card.Description>
+          <Link to ={`/event/${event.id}`}>
+            View Details
+          </Link>
+        </Card.Content>
+        <Card.Content extra>
+          { this.eventTime(event)}
+        </Card.Content>
+      </Card>
+    )
+  })
+}
+
   render() {
     return(
-      <Header as="h1" textAlign="center">Events</Header>
+      <Container>
+        <Card.Group stackable itemsPerRow={3}>
+          {this.displayEvents()}
+        </Card.Group>
+      </Container>
     );
   }
 }
