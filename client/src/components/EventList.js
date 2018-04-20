3250e13b6ad30fa0835f2ae453d4fe6b726050f4
Brock Beuch <brockbeuch@gmail.com>
2018-04-19 19:12:55 -0600

    added xp
    Reviewers: #events_page, JonnyUtah23
    
    Reviewed By: #events_page, JonnyUtah23
    
    Subscribers: JonnyUtah23
    
    Differential Revision: http://phabricator.devpointstudios.com/D1920

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { List, Header, Card, Container, GridColumn, Grid, Image } from 'semantic-ui-react';
+import { List, Header, Card, Container, GridColumn, Grid, Image, Icon, Divider, Progress } from 'semantic-ui-react';
 import { Redirect, Link } from 'react-router-dom';
 // import { isAuthenticated } from '../fakeAuth';
 import  Events from './Events';
@@ -23,8 +23,8 @@ class EventList extends React.Component {
       <Grid centered>
         <Grid.Row>
           <Grid.Column width={8} style={{ textAlign: 'center'}}>
+            <Icon name='calendar' textAlign='middle' />
             Date: { event.date}
-
           </Grid.Column>
           <Grid.Column width={8} style={{ textAlign: 'center'}}>
             Time: {event.start_time} - {event.end_time}
@@ -39,29 +39,30 @@ class EventList extends React.Component {
   return this.state.events.map( event => {
     return(
 
-      <Card>
+      <Card color="purple">
+        <Image src={ event.event_image } />
         <Card.Content>
           <Card.Header>
-            {event.title}
-            <hr />
-            {event.category}
+            <Link to='/events/:id'>
+              {event.title}
+            </Link>
           </Card.Header>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <Image src={ event.event_image } />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <Card.Meta>
+            <span className='date'>
+              { event.date}
+            </span>
+          </Card.Meta>
           <Card.Description>
             {event.description}
           </Card.Description>
-          <Link to ={`/event/${event.id}`}>
-            View Details
-          </Link>
         </Card.Content>
-        <Card.Content extra>
-          { this.eventTime(event)}
+        <Card.Content extra textAlign='centered'>
+
+            { this.eventTime(event)}
+          <Divider/>
+          <Progress percent={event.xp} size='tiny'>
+            XP: {event.xp}
+          </Progress>
         </Card.Content>
       </Card>
     )
---
+++ client/src/components/logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const Logo = () => {
+  <Image src='client/src/assets/images/DPL-logo-color@2x.png' size='small' />
+}
+
+export default Logo
